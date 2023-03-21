a56fd68261d4a06f3c173a5de67a93c58fde8794
Robert Pinl <robert.pinl@icloud.com>
2023-03-21 20:23:13 +1300

    Add async fetch api

--- BreakingNews/Model/NewsAPIManager.swift
+++ BreakingNews/Model/NewsAPIManager.swift
@@ -16,33 +16,42 @@ struct NewsAPIManager {
     
     func fetchAPI() async throws-> [Article] {
         
+        guard let url = URL(string: "") else {
+            throw ApiError.invalidUrl
+        }
+        
+        let (data,_) = try await URLSession.shared.data(from: url)
+        
+        let articles = try JSONDecoder().decode([Article].self, from: data)
+        
+        return articles
     }
     
-    func fetchAPI(urlcompletion: @escaping (Result<[Article], Error>) -> Void) {
-        if let url = URL(string: urlString) {
-            let session = URLSession(configuration: .default)
-            let task = session.dataTask(with: url) { (data, response, error) in
-                if error != nil {
-                    completion(.failure(error!))
-                    return
-                }
-                if let safeData = data {
-                    let decoder = JSONDecoder()
-                    do {
-                        let articles = try decoder.decode(NewsModel.self, from: safeData).articles
-                        completion(.success(articles))
-                    } catch {
-                        print(error)
-                    }
-                }
-            }
-            task.resume()
-        }
-    }
+//    func fetchAPI(urlcompletion: @escaping (Result<[Article], Error>) -> Void) {
+//        if let url = URL(string: urlString) {
+//            let session = URLSession(configuration: .default)
+//            let task = session.dataTask(with: url) { (data, response, error) in
+//                if error != nil {
+//                    completion(.failure(error!))
+//                    return
+//                }
+//                if let safeData = data {
+//                    let decoder = JSONDecoder()
+//                    do {
+//                        let articles = try decoder.decode(NewsModel.self, from: safeData).articles
+//                        completion(.success(articles))
+//                    } catch {
+//                        print(error)
+//                    }
+//                }
+//            }
+//            task.resume()
+//        }
+//    }
 }
 
 enum ApiError: Error {
-    case invalidURL
+    case invalidUrl
     case missingData
 }
 
